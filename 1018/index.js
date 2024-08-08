dc7d5bb713a168a14ce70c24a16191a8b51e0520
JMyang30 <didwoans30@gmail.com>
2024-08-08 11:40:42 +0900

    체스판 다시 칠하기

--- 1018/index.js
+++ 1018/index.js
@@ -4,7 +4,7 @@ const input = require("fs")
   .trim()
   .split("\r\n");
 
-const [y, x] = input.shift().split(" ");
+const [y, x] = input.shift().split(" ").map(Number);
 
 const BOARD = [];
 const SIZE = { x: 8, y: 8 };
@@ -17,9 +17,9 @@ const paintCount = (startColor, startRow, startCol) => {
   let count = 0;
   let nextColor = startColor;
 
-  for (let i = 0; i < startRow + SIZE.x; i++) {
-    for (let j = 0; j < startCol + SIZE.y; j++) {
-      if (BOARD[i][j] !== startColor) {
+  for (let i = startRow; i < startRow + SIZE.x; i++) {
+    for (let j = startCol; j < startCol + SIZE.y; j++) {
+      if (BOARD[i][j] !== nextColor) {
         count++;
       }
       nextColor = nextColor === "B" ? "W" : "B";
@@ -32,6 +32,16 @@ const paintCount = (startColor, startRow, startCol) => {
 
 let result = SIZE.x * SIZE.y;
 
-for (let i = 0; i < y - SIZE.y; i++) {
-  for (let j = 0; j < x - SIZE.x; j++) {}
+for (let i = 0; i <= y - SIZE.y; i++) {
+  for (let j = 0; j <= x - SIZE.x; j++) {
+    const black = paintCount("B", i, j);
+    const white = SIZE.x * SIZE.y - black;
+    const min = black > white ? white : black;
+
+    if (result > min) {
+      result = min;
+    }
+  }
 }
+
+console.log("result:", Math.abs(result));
